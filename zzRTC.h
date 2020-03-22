81103764064c70bfb948fece737dd5c92f050d29
hohno-46466 <hohno.46466@gmail.com>
2020-03-22 17:51:41 +0900

    committed by @hohno_at_kuimc

--- zzRTC.h
+++ zzRTC.h
@@ -36,45 +36,45 @@
 
 class zzRTC {
  public:
-	zzRTC();
+  zzRTC();
   //
   void begin();
-	//
+  //
   uint8_t isrunning(void);
   boolean startClock();
   boolean stopClock();
-	//
+  //
   void setTime(void);
   void getTime(void);
-	//
+  //
 #ifdef BOGUS_BOGUS_ZZRTC
-	boolean fillByHMS(uint8_t, uint8_t, uint8_t);
+  boolean fillByHMS(uint8_t, uint8_t, uint8_t);
   boolean fillByY2kMD(uint16_t, uint8_t, uint8_t);
   boolean fillDayOfWeek(int8_t);
 #endif // BOGUS_BOGUS_ZZRTC
-	//
-	uint8_t year;    // 0..100 (0->2000, 100->2100)
+  //
+  uint8_t year;    // 0..100 (0->2000, 100->2100)
   uint8_t month;
   uint8_t day;
   uint8_t dayOfWeek;// day of week, 1 = Monday
-	uint8_t hour;
-	uint8_t minute;
-	uint8_t second;
+  uint8_t hour;
+  uint8_t minute;
+  uint8_t second;
 #ifdef BOGUS_BOGUS_ZZRTC
   uint8_t control;
   uint8_t ram0;
   uint8_t ram1;
   uint8_t ram2;
 #endif // BOGUS_BOGUS_ZZRTC
-	//
+  //
  private:
-	uint8_t _zzYear;
+  uint8_t _zzYear;
   uint8_t _zzMonth;
   uint8_t _zzDay;
   uint8_t _zzDayOfWeek;
-	uint8_t _zzHour;
-	uint8_t _zzMinute;
-	uint8_t _zzSecond;
+  uint8_t _zzHour;
+  uint8_t _zzMinute;
+  uint8_t _zzSecond;
 };
 
 // ---------------------------------------------------------
